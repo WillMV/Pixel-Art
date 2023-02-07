6305db0fedbb36eb6bbfdf0a7005f0ee29aca55f
Max Lima <max.lima2@gmail.com>
2023-02-06 22:10:00 -0300

    feat: add logic to paint on click and drag

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ color[1].style.backgroundColor = 'red';
 color[2].style.backgroundColor = 'green';
 color[3].style.backgroundColor = 'blue';
 
+let isMousePressed = false
+
 function restoreColorPalette() {
   if (localStorage.colorPalette) {
     const palette = JSON.parse(localStorage.colorPalette);
@@ -139,9 +141,24 @@ if (localStorage.boardSize) {
   newBoard();
 }
 
+function handleMouseMove(event) {
+  if (isMousePressed) {
+    paint(event)
+  }
+}
+
+function handleMouseDown(event) {
+  isMousePressed = true
+}
+
+function handleMouseUp(event) {
+  isMousePressed = false
+}
+
 colorPalette.addEventListener('click', selector);
 clearBoard.addEventListener('click', erase);
-pixelBoard.addEventListener('click', paint);
+pixelBoard.addEventListener('mousedown', paint)
+pixelBoard.addEventListener('mousemove', handleMouseMove)
 colorButton.addEventListener('click', colorGenerator);
 hasBorder.addEventListener('click', newBoard)
 boardBtn.addEventListener('click', () => {
@@ -149,3 +166,6 @@ boardBtn.addEventListener('click', () => {
   newBoard()
 });
 
+this.addEventListener('mousedown', handleMouseDown)
+this.addEventListener('mouseup', handleMouseUp)
+this.addEventListener('mouseleave', handleMouseUp)
